bc3d61975120dd5b470c9255001bb298df7c7d8e
Saketh Macharla <saketh.macharla@zemosolabs.com>
2022-09-07 17:54:11 +0530

    component lifecycle assignment

---
+++ LifeCycle-Assignment-1/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import UserDetails from './components/UserDetails';
+
+function App() {
+  return (
+    <UserDetails />
+  );
+}
+
+export default App;
